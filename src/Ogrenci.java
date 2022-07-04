a14df495214877c1f67ead8d9b4c1ab93c587f66
Ceyo907 <ceytas455@gmail.com>
2022-07-04 21:48:28 +0300

    Öğrenci Class ı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,7 +1,9 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dünya");
 
+
+        System.out.println("Merhaba Dünya2");
+        System.out.println("Merhaba Dünya2");
 
 
     }
